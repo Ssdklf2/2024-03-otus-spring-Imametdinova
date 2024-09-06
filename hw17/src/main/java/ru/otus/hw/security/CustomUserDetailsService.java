a7de9a4f73f7da15252797da357dd39531d7cdea
Imametdinova Irina <iri.imametdinova@x5.ru>
2024-09-06 14:41:47 +0300

    old hw

---
+++ hw17/src/main/java/ru/otus/hw/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package ru.otus.hw.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.NotFoundException;
+import ru.otus.hw.repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new NotFoundException("User %s not found".formatted(username)));
+    }
+}
